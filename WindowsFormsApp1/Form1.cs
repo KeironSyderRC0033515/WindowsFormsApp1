679ed4a1fac603c3c9344d35aad7fc2cecd0ec2f
rc0033515 <rc0033515@CRO-24-01>
2018-09-11 10:39:57 +0100

    right and left working

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -26,5 +26,10 @@ namespace WindowsFormsApp1
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
     }
 }
