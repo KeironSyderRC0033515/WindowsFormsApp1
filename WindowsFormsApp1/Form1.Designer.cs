8b4970e7b5e5b8a886fe27ab528510ff6cd64e8e
rc0033515 <rc0033515@CRO-24-01>
2018-09-17 15:09:56 +0100

    all borders set

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.Right = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.Up = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,55 +46,55 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // Right
             // 
-            this.button1.Location = new System.Drawing.Point(148, 337);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Right ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Right.Location = new System.Drawing.Point(148, 337);
+            this.Right.Name = "Right";
+            this.Right.Size = new System.Drawing.Size(75, 23);
+            this.Right.TabIndex = 1;
+            this.Right.Text = "Right ";
+            this.Right.UseVisualStyleBackColor = true;
+            this.Right.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // left
             // 
-            this.button2.Location = new System.Drawing.Point(67, 337);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Left";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.left.Location = new System.Drawing.Point(67, 337);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(75, 23);
+            this.left.TabIndex = 2;
+            this.left.Text = "Left";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // Up
             // 
-            this.button3.Location = new System.Drawing.Point(107, 308);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Up";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.Up.Location = new System.Drawing.Point(107, 308);
+            this.Up.Name = "Up";
+            this.Up.Size = new System.Drawing.Size(75, 23);
+            this.Up.TabIndex = 3;
+            this.Up.Text = "Up";
+            this.Up.UseVisualStyleBackColor = true;
+            this.Up.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // down
             // 
-            this.button4.Location = new System.Drawing.Point(107, 366);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Down";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.down.Location = new System.Drawing.Point(107, 366);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(75, 23);
+            this.down.TabIndex = 4;
+            this.down.Text = "Down";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.Up);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.Right);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -107,10 +107,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Right;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button Up;
+        private System.Windows.Forms.Button down;
     }
 }
 
